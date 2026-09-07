9de30540232491ecaa3f8a15c81290d44e52e456
andsu-dev <alucasm02@gmail.com>
2026-09-07 14:32:23 -0300

    build(cask): bump to 0.5.0

--- Casks/speech-md.rb
+++ Casks/speech-md.rb
@@ -1,6 +1,6 @@
 cask "speech-md" do
-  version "0.4.0"
-  sha256 "026782b9d2f92fbec67b5ccc8caaef671f5b042fcfc3cc0fa754baa4a6120cf5"
+  version "0.5.0"
+  sha256 "944c5dae5aa936855312ec737a9e071a0ee96e86f7142a3e65ca9ef0a01ad222"
 
   url "https://github.com/Andsu-dev/speech.md/releases/download/v#{version}/speech.md-#{version}.zip"
   name "speech.md"
